7a0b3100f2255fd2669efb948ad599c45069f267
Alex Marquez <amarq2376@gmail.com>
2024-03-08 11:48:52 -0500

    Initial Commit

---
+++ goal-tracker/goal_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  goal_trackerApp.swift
+//  goal-tracker
+//
+//  Created by Alex Marquez on 3/8/24.
+//
+
+import SwiftUI
+
+@main
+struct goal_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
